ec1f7e7ddea9ac642372fd33a4124f2511637904
Michael Shervashidze <mike.fury315@gmail.com>
2025-03-11 16:38:41 +0700

    средняя панель добавил, правая визуал готова

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,4 @@
+//---------Работа с картой---------
 ymaps.ready(init);
 function init() {
     var myMap = new ymaps.Map("map", {
@@ -91,4 +92,46 @@ function init() {
         leftPanel.classList.toggle('hidden');
         leftPanel.classList.contains('hidden') ? imgChevron.setAttribute("src", "/assets/chevron_right.svg") : imgChevron.setAttribute("src", "/assets/chevron_left.svg");
     });
+
+
+    function  Select() {
+        let selectHeader = document.querySelectorAll('.select__header');
+        let selectItem = document.querySelectorAll('.select__item');
+
+        selectHeader.forEach(item => {
+            item.addEventListener('click', selectToggle)
+        });
+
+        selectItem.forEach(item => {
+            item.addEventListener('click', selectChoose)
+        });
+
+        document.addEventListener('click', function(event) {
+            let dropdown = document.querySelector('.select.is-active');
+            if (dropdown && !dropdown.contains(event.target)) {
+                dropdown.classList.remove('is-active');
+            }
+        });
+
+        function selectToggle() {
+            this.parentElement.classList.toggle('is-active');
+        }
+
+        function selectChoose() {
+            let text = this.innerText,
+                select = this.closest('.select'),
+                currentText = select.querySelector('.select__current');
+            currentText.innerText = text;
+            select.classList.remove('is-active');
+            select.classList.add('is-chosen');
+        }
+
+    };
+    let select = document.querySelector('.select');
+    Select();
 }
+
+//--------Работа с выпадающим списком
+
+
+
